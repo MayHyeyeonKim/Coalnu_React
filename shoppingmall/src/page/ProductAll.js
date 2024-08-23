bade50f77ed1ae8a1366bd8e89704be21218b58b
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-23 01:24:56 -0700

    changed json-server url for deployment

--- shoppingmall/src/page/ProductAll.js
+++ shoppingmall/src/page/ProductAll.js
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Response } from '../../node_modules/whatwg-fetch/fetch';
 import ProductCard from "../component/ProductCard";
 import { Container, Row, Col } from 'react-bootstrap';
-import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
-    const [query, setQuery] = useSearchParams();
     const getProducts = async()=>{
-        // let url = `http://localhost:4000/products`
-        let searchQuery = query.get('q') || '';
-        const url = `https://my-json-server.typicode.com/mayhyeyeonkim/react-coalnu/shoppingmall/products?q=${searchQuery}`;
+        const url = `https://my-json-server.typicode.com/mayhyeyeonkim/react-coalnu/shoppingmall/products`;
         let response = await fetch(url)
         let data = await response.json();
         setProductList(data);
     }
     useEffect(()=>{
         getProducts();
-    },[query])
+    },[])
     return <div>
         <Container>
             <Row>

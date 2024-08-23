00c1b449c0dac716edd857770054e0c346d5fdf0
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-23 01:10:56 -0700

    changed json-server url for deployment

--- shoppingmall/src/page/ProductAll.js
+++ shoppingmall/src/page/ProductAll.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Response } from '../../node_modules/whatwg-fetch/fetch';
 import ProductCard from "../component/ProductCard";
 import { Container, Row, Col } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
+    const [query, setQuery] = useSearchParams();
     const getProducts = async()=>{
         // let url = `http://localhost:4000/products`
         let url = `https://my-json-server.typicode.com/mayhyeyeonkim/react-coalnu/shoppingmall/products?q=${searchQuery}`
